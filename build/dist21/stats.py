2af53af5b3f13a9b6312bb0951a4a9cfcbf2c1dd
George L. F. Harris <glfharris@gmail.com>
2018-09-16 00:32:42 +0100

    Cleared build files

--- build/dist21/stats.py
+++
@@ -1,7 +0,0 @@
-from aqt import mw
-
-def note_count(deck, tag=''):
-    return mw.col.db.scalar('select count() '
-                            'from cards inner join notes '
-                            'on cards.nid = notes.id '
-                            'where cards.did = "' + deck + '" and notes.tags like "%' + tag + '%"')
