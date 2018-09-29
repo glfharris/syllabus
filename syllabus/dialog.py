32acb19a1b38f23e14111345572b55baca2e9023
George L. F. Harris <glfharris@gmail.com>
2018-09-29 14:00:00 +0100

    Fix resizing

--- syllabus/dialog.py
+++ syllabus/dialog.py
@@ -10,12 +10,14 @@ from .node import *
 from .tree import SyllabusTreeView
 from .ui_dialog import Ui_Syllabus
 
+config = mw.addonManager.getConfig(__name__)
+
 class SyllabusDialog(QDialog, Ui_Syllabus):
 
     def __init__(self, mw):
         super().__init__()
         self.setupUi(self)
-        self.tree_view = SyllabusTreeView(cols=DEFAULT_COLUMNS)
+        self.tree_view = SyllabusTreeView(cols=config['last_columns'])
 
         self.horizontalLayout.addWidget(self.tree_view, 60)
 
@@ -26,19 +28,23 @@ class SyllabusDialog(QDialog, Ui_Syllabus):
         self.export_btn.clicked.connect(self.output_tree)
         self.apply_col_settings.clicked.connect(self.apply_settings)
 
-        tree_size = self.tree_view.viewportSizeHint()
-        group_size = self.groupBox.sizeHint()
+        self.resize_window()
 
-        size = QSize(tree_size.width() + group_size.width(), tree_size.height() * 1.1)
-        
-        self.resize(size)
         self.show()
         self.activateWindow()
     
+    def resize_window(self):
+        tree_size = self.tree_view.viewportSizeHint()
+        group_size = self.groupBox.sizeHint()
+
+        size = QSize(tree_size.width() + group_size.width(), tree_size.height() + 0.5 * group_size.height())
+        
+        self.resize(size)
+    
     def populate_column_settings(self):
-        column_dict = {'Total': TOTAL, 'New': NEW, 'Learning': LEARNING, 'Mature': MATURE, 'Ease': EASE,
-                       'Retention': {'Young Retention': YOU_RENT, 'Mature Retention': MAT_RENT, 'Total Retention': TOT_RENT},
+        column_dict = {'Total': TOTAL, 'New': NEW, 'Learning': LEARNING, 'Young': YOUNG, 'Mature': MATURE, 'Ease': EASE,
                        'Unqueued': {'Buried': BURIED, 'Suspended': SUSPENDED},
+                       'Retention': {'Young Retention': YOU_RENT, 'Mature Retention': MAT_RENT, 'Total Retention': TOT_RENT},
                        'Misc': {'Kind': KIND, 'Deck': DECK}}
         model = QStandardItemModel()
         model.setHorizontalHeaderLabels(['Column'])
@@ -55,12 +61,12 @@ class SyllabusDialog(QDialog, Ui_Syllabus):
                     child_tmp = QStandardItem(child_name)
                     child_tmp.setCheckable(True)
                     child_tmp.setData(child_key, Qt.UserRole)
-                    if child_key in DEFAULT_COLUMNS:
+                    if child_key in config['last_columns']:
                         child_tmp.setCheckState(Qt.Checked)
                     tmp.appendRow(child_tmp)
             else:
                 tmp.setData(key, Qt.UserRole)
-                if key in DEFAULT_COLUMNS:
+                if key in config['last_columns']:
                     tmp.setCheckState(Qt.Checked)
             parent_item.appendRow(tmp)
         
@@ -92,6 +98,9 @@ class SyllabusDialog(QDialog, Ui_Syllabus):
         
         cols = _recur_check(self.col_tree.root, res=[0])
         self.tree_view.gen_tree(cols=cols)
+        self.resize_window()
+        config['last_columns'] = cols
+        mw.addonManager.writeConfig(__name__, config)
     
     def output_tree(self):
         path, _ = QFileDialog.getSaveFileName(self,"Export Syllabus Tree","tmp.csv","CSV Files (*.csv)")
